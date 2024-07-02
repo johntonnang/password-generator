638e601412cba50cb172c996a619184e801659f3
John Tönnäng <john_tonnang@hotmail.com>
2024-07-02 14:27:52 +0200

    Add fonts, icons and setup developer configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import icon from "astro-icon";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://example.com",
+  integrations: [react(), icon()],
+});
